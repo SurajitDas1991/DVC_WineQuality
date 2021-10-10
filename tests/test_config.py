b35c17cdc774e73aa3b767a9a22d51e860be263a
SurajitDas1991 <43229889+SurajitDas1991@users.noreply.github.com>
2021-10-10 19:17:02 +0530

    Added a notebook for test cases

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,2 +1,12 @@
+import pytest
+class NotInRange(Exception):
+    def __init__(self,message="value not in range"):
+        #self.input=input_
+        self.message=message
+        super().__init__(self.message)
+
 def test_generic():
-    assert 2==2
+    a=5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange

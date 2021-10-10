4c430f502e8f51ae261b99a4579730c38ea9ed98
SurajitDas1991 <43229889+SurajitDas1991@users.noreply.github.com>
2021-10-10 18:37:42 +0530

    Added setup.py ,tox and pytest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It is a Wine Quality package with DVC",
+    author="Surajit",
+    packages=find_packages(),
+    license="MIT"
+)
+
